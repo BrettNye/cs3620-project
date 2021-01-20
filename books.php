02bedcd45c30a3a93988cb6c0b11c19058d44f32
BrettNye <43053436+BrettNye@users.noreply.github.com>
2021-01-19 19:10:42 -0700

    Update books.php

--- books.php
+++ books.php
@@ -3,13 +3,4 @@ header("Access-Control-Allow-Origin: *");
 ?>
 
 
-{"books-read":[
-    {
-        "Title": "My side of the Mountain",
-        "Author": "Jean Craighead George" 
-    },
-    {
-        "Title": "The Odyssey",
-        "Author": "Homer"
-    }
-]}
+{"books-read":[{"Title": "My side of the Mountain","Author": "Jean Craighead George" },{"Title": "The Odyssey","Author": "Homer"}]}
