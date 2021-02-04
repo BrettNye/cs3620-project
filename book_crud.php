14a74cb387cdce735043faf46ca74a8eebe720d6
brettnye <brettnye@mail.weber.edu>
2021-02-03 21:42:12 -0700

    Update book_crud.php

--- book_crud.php
+++ book_crud.php
@@ -2,10 +2,8 @@
 session_start();
 
 $servername = "nyecs3620database.mysql.database.azure.com";
-//$username =  (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
-//$password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
-$username = "nyeAdmin@nyecs3620database";
-$password = "Combustioniskey9";
+$username =  (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
+$password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
 $dbname = "cs3620_proj";
 
 // Create connection
