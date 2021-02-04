00adabf84b0a962b224d9155fd446f6bb9bfc076
brettnye <brettnye@mail.weber.edu>
2021-02-03 21:41:48 -0700

    Update book_crud.php

--- book_crud.php
+++ book_crud.php
@@ -2,9 +2,11 @@
 session_start();
 
 $servername = "nyecs3620database.mysql.database.azure.com";
-$username =  (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
-$password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
-$dbname = "cs3620";
+//$username =  (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
+//$password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
+$username = "nyeAdmin@nyecs3620database";
+$password = "Combustioniskey9";
+$dbname = "cs3620_proj";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -37,14 +39,14 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "<br>id: " . $row["book_id"]. " Title: " . $row["Title"]. " Author " . $row["Author"]. "<br>";
   }
 } else {
   echo "0 results";
 }
 $conn->close();
 
-echo $result;
+//echo $result;
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -53,12 +55,28 @@ if ($conn->connect_error) {
 }
 
 // sql to delete a record
-$sql = "DELETE FROM books WHERE id=1";
+$sql = "DELETE FROM books WHERE book_id=1";
 
 if ($conn->query($sql) === TRUE) {
   echo "Record deleted successfully";
 } else {
   echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to delete a record
+$sql = "DELETE FROM books";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<br>Record deleted successfully";
+} else {
+  echo "<br>Error deleting record: " . $conn->error;
 }
 
 $conn->close();
